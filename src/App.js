907b00a49f9e9a30ae1f5f6cba59dbfe5d7b4ba9
jequezinho <jroverby92@gmail.com>
2020-08-24 12:47:17 -0500

    ...

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+        Essa aqui é a segunda versão do Appe!
         </p>
         <a
           className="App-link"
@@ -16,7 +16,6 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Essa aqui é a segunda versão do Appe!
         </a>
       </header>
     </div>
