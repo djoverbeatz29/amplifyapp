ba9035acd7cf88b52eb770795bf34108ec87197e
jequezinho <jroverby92@gmail.com>
2020-08-24 12:45:43 -0500

    Umas mudanças

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Essa aqui é a segunda versão do Appe!
         </a>
       </header>
     </div>
